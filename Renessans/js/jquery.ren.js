5411e4ff0bd4392939642348dd91d9d96ee222f1
Nastya <sirena857@gmail.com>
2017-02-22 23:20:40 +0200

    ren with new table_nav_menu + fancybox

--- Renessans/js/jquery.ren.js
+++ Renessans/js/jquery.ren.js
@@ -5,3 +5,4 @@ $("div.content p > a").addClass("info_links");
 $("div.period_parts > h3").addClass("part_name");
 
 $("div.content a > img.min_img").addClass("response_img");
+
--- Renessans/js/tabs.js
+++ Renessans/js/tabs.js
@@ -3,12 +3,23 @@
  */
 (function($) {
     $(function() {
-
         $('ul.navigation').on('click', 'li:not(.active)', function() {
             $(this)
                 .addClass('active').siblings().removeClass('active')
-                .closest('div.tabs').find('div.tabs__content').toggleClass('active').eq($(this).index());
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
         });
 
     });
 })(jQuery);
+
+(function($) {
+    $(function() {
+        $('table.navigation').on('click', 'td:not(.active)', function() {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
+    });
+})(jQuery);
+
